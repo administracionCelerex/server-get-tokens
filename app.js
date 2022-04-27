8f3421f464ed6f6d9b4fee3d82dcf2093cb3ba9a
luissevilla <luissevilla111@gmail.com>
2022-04-27 14:54:27 -0500

    fixing url

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ const { google } = require("googleapis");
 const oauth2Client = new google.auth.OAuth2(
   process.env.GOOGLE_CLIENT_ID,
   process.env.GOOGLE_CLIENT_SECRET,
-  "http:localhost:8080/handleGoogleRedirect" // server redirect url handler
+  "http://localhost:8080/handleGoogleRedirect" // server redirect url handler
 );
 const fetch = require("node-fetch");
 
